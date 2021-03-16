22d1d35d069972e95303e9eb102c68741c8f7915
trinhtoai <79903575+trinhtoai@users.noreply.github.com>
2021-03-16 17:06:24 +0700

    Add files via upload

---
+++ b11.py
@@ -0,0 +1,7 @@
+def benefit(t,n,k):
+    t=int(input("lai suat tiet tiet kiem"))
+    n=int(input("nhap so von ban dau"))
+    k=int(input("so thang gui"))
+    so tien=n*t*k
+print(so tien)
+benefit(100,4,5)
